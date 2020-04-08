d336e61447911e853eed8bd3d6f020798b8405f2
MickeyShiue <e031304@gmail.com>
2020-04-08 15:37:05 +0800

    init

---
+++ MyService.Lib.Tests/CategoryServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyService.Lib.Tests
+{
+    [TestFixture]
+    public class CategoryServiceTests
+    {
+        [Test]
+        public void Create_傳入不存在的分類名稱_新增資料成功()
+        {
+            string categoryName = "BBB";
+
+            ICategoryDAO dao = Substitute.For<ICategoryDAO>();
+            dao.IsExist(Arg.Any<string>()).Returns(false);
+
+            var service = new CategoryService(dao);
+            service.Create(categoryName);
+
+            dao.Received().Create(categoryName);
+        }
+
+        [Test]
+        public void Create_傳入已存在的分類名稱_拋出例外()
+        {
+            string categoryName = "BBB";
+
+            ICategoryDAO dao = Substitute.For<ICategoryDAO>();
+            dao.IsExist(Arg.Any<string>()).Returns(true);
+
+            var service = new CategoryService(dao);
+            var ex = Assert.Throws<Exception>(() => service.Create(categoryName));
+
+            Assert.That(() => ex.Message.Contains("存在"));
+        }
+    }
+}
---
+++ MyService.Lib.Tests/OrderServiceTest.cs
@@ -0,0 +1,66 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using System;
+
+namespace MyService.Lib.Tests
+{
+    [TestFixture]
+    public class OrderServiceTest
+    {
+        [Test]
+        public void Checkout_訂單金額過低_拋出例外()
+        {
+            var oredeContext = new OrderEntity() { TotalCost = 99 };
+            IShippingCostProvider provider = Substitute.For<IShippingCostProvider>();
+
+            var service = new OrderService(provider);
+            var ex = Assert.Throws<Exception>(() => service.Checkout(oredeContext));
+
+            Assert.That(() => ex.Message.Contains("無法出貨"));
+        }
+
+        [Test]
+        public void Checkout_訂單金額過高_拋出例外()
+        {
+            var oredeContext = new OrderEntity() { TotalCost = 250000 };
+            IShippingCostProvider provider = Substitute.For<IShippingCostProvider>();
+
+            var service = new OrderService(provider);
+            var ex = Assert.Throws<Exception>(() => service.Checkout(oredeContext));
+
+            Assert.That(() => ex.Message.Contains("無法線上購物"));
+        }
+
+        [Test]
+        public void Checkout_郵寄正常出貨()
+        {
+            var oredeContext = new OrderEntity() { TotalCost = 1000, ShippingMethod = EnumShippingMethod.PostOffice };
+            IShippingCostProvider provider = Substitute.For<IShippingCostProvider>();
+            provider.GetShippingCost(Arg.Any<OrderEntity>()).Returns(80);
+
+            var service = new OrderService(provider);
+            service.Checkout(oredeContext);
+
+            int actual = oredeContext.TotalWithShippingCost;
+            int expected = 1080;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Checkout_黑貓正常出貨()
+        {
+            var oredeContext = new OrderEntity() { TotalCost = 1000, ShippingMethod = EnumShippingMethod.PostOffice };
+            IShippingCostProvider provider = Substitute.For<IShippingCostProvider>();
+            provider.GetShippingCost(Arg.Any<OrderEntity>()).Returns(100);
+
+            var service = new OrderService(provider);
+            service.Checkout(oredeContext);
+
+            int actual = oredeContext.TotalWithShippingCost;
+            int expected = 1100;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
